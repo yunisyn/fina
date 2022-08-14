ca407e7418b799bcb657336ca267b2eb741c1212
unknown <yunisyn>
2022-08-14 06:49:27 -0700

    hwww

---
+++ program hw.cs
@@ -0,0 +1,268 @@
+﻿using System;
+
+namespace OPPP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a;
+            //int b;
+            //int c;
+            //int result = 1;
+            //for (a > 0; a % 6 == 0)
+            //{
+            //    result*=a;
+            //}
+            //else (b > 0); b % 6 == 0)
+            //    {
+            //    result*=b;
+
+            //}
+            //else if (c > 0; c % 6 == 0)
+            //    {
+            //    result *=c;
+            //}
+            //Console.WriteLine(result);
+
+
+
+            // 1 - dən M - ədək ədədlər içərisində 21 - ə bölünən ədədlərin ədədi ortasını tapan alqoritm
+            //int[] numbers;
+            //int m;
+            //int counter=0;
+            //int sum =0; 
+            //for (int i=0; i<numbers.Length; i++)
+            //{
+            //    numbers[i] = counter++;
+            //    sum+= numbers[i];
+            //    counter++;  
+           //}
+            //if (counter != 0)
+            //{
+             //   int result = sum/counter;
+             //   Console.WriteLine(Result);
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Verilmiş M-dən N - ədək ədədlər içərisində neçə ədədin rəqəmləri cəminin cüt olduğunu tapan alqoritm
+            //int n;
+            //int m;
+            //int sum = 0;
+            //Console.WriteLine();
+            //string sentence = Console.ReadLine();
+            //Console.WriteLine();
+            //string letterStr = Console.ReadLine();
+            //int counter = 0;
+            //int numbers = {1,2,3,4,5};
+            //for (int i = 0; i < numbers; i++)
+            //{
+            //    m = i;
+            //   n = i;
+            //   sum = 0;
+            //}
+            //else
+            //{
+            //    Console.WriteLine(numbers);
+            //}
+
+
+
+
+
+
+
+
+
+
+            //Verilmiş M ədədindən verilmiş N ədədinədək olan ədədlərin cəmini tapan alqoritm
+            //int n;
+            //int m;
+            //int sum;
+            //for (n = 0; n < args.Length; n++) 
+            //{
+            //    if (args[n].Length == 0)
+            //    {
+            //        sum = 0;
+            //    }
+            //    else { sum = 0; }
+            //    {
+            //        n = 0;
+
+            //    }
+            // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // 1 - dən M - ədək ədədlər içərisində 15 - ə bölünən ədədlərin sayını tapan alqoritm
+            //int[] numbers = { 1, 5, 20, 30, 10 };
+            //int count = 0;
+            // 
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 15 == 0){
+            //        count++;
+            //    }
+            //    
+            //    
+            //}
+            //if (count != 0)
+            //{
+            //    int result = numbers / count;
+            //    Console.WriteLine(result);
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Verilmiş adlar siyahısındalki bütün sözlərin 'A' hərfi ilə başlayıb başlamadığını tapan proqram.Bütün sözlər A ilə başlayırsa console-a başlayır, başlamırsa başlamır yazılmalıdır nəticə olaraq
+            //string word;
+            //bool hasWantedLetter = false;
+            //string word = Console.ReadLine();
+            //
+            //
+            //do
+            //{
+            //    word = Console.ReadLine();
+            //    for (int i = 0; i < word.Length; i++)
+            //    {
+            //        if (word[i] == 'a')
+            //        {
+            //            hasWantedLetter = true;
+            //            break;
+            //        }
+            //    }
+            //} while (hasWantedLetter == false);
+
+
+
+            //Console.WriteLine("bashlamir!");
+
+
+
+
+
+
+
+
+
+
+
+            //Console - dan bir say daxilir və daha sonra həmin say qədər ad daxil edilir.Yekun olaraq console - dan arrayın uzunluğunu qəbul edib o sayda ad - ı da console-dan götürüb array-ə yerləşdimək lazımdır
+
+
+
+
+            //Consoledan istifadəçi adı daxil edilir.Əgər istifadəçi adı rəqəmlə başlayırsa və ya içində 4 - dən az char varsa yenidən daxil edilməsini istəməlidir.
+            //string word = Console.ReadLine();
+            //string letterStr = Console.ReadLine();
+            //bool hasWantedLetter = false;
+            //char letter = Convert.ToChar(letterStr);
+            //do
+            //
+            //    for (int i = 0; i < word.Length; i++)
+            //    {
+            //        if (word[i] == letter)
+            //            hasWantedLetter = true;
+            //        break;
+            //    }
+            //while (hasWantedLetter == false);
+            //{
+            //    Console.WriteLine("bitti");
+            //}
+
+
+
+
+
+
+
+
+            // Console - dan 5 ədəd qəbul edib bu ədədlərin ədədi ortasını hesablayan proqram
+            //int[] numbers = { 1, 2, 3, 4, 5, 6 };
+            //int[] sum = 0;
+            //int count = 0;
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] != sum[i]) ;
+            //    count++;
+            //}
+            //if (count != 0)
+            //{
+            //    int result = sum / count;
+            //    Console.WriteLine(result);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("ededin ortasindaki say");
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Console - dan daxil edilmiş yazıda rəqəm olub olmadığını tapan proqram.Rəqəm varsa rəqəm var yazılmalıdır, yoxdursa yoxdur yazılmalıdır
+            //string word = "yunis1";
+            //bool hasWantedLetter;
+
+            //for (int i = 0; i < word.Length; i++)
+            //{
+            //  if(word[i] == '1' )
+            //    {
+            //        hasWantedLetter = true;
+            //        break;
+            //    }
+            //}
+            //if (hasWantedLetter == true )
+            //{
+            //    Console.WriteLine(" reqem var ");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(" reqem yoxdur ");
+            //}
+
+        }
+    }
+}
